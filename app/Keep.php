ba26f90be39091b395a8d3c24aa95c883a0ebf7c
Italo Morales F <i@italomoralesf.com>
2016-03-20 15:13:05 -0500

    install database

---
+++ app/Keep.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Keep extends Model
+{
+    //
+}
---
+++ database/migrations/2016_03_20_200236_create_keeps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateKeepsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('keeps', function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->string('keep');
+            $table->integer('user_id')->unsigned();
+            
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('keeps');
+    }
+}
